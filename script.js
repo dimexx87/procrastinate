c5162e8ab86a91abeab310049c47be53899d16b3
Dmitriy Gibadullin <dimexx87@yandex.ru>
2020-05-01 17:52:34 +0300

    4

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* file: script.js*/
-
 let phrases = [
   { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
   { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
@@ -19,24 +17,22 @@ function getRandomElement(arr) {
 }
 
 let button = document.querySelector('.button');
-let phrase = document.querySelector('.phrase');
 let advice = document.querySelector('.advice');
+let phrase = document.querySelector('.phrase');
 let image = document.querySelector('.image');
+
+for (let i = 0; i<=1; i++) {
+  smoothly(phrase, 'textContent', phrases[i].text);
+  smoothly(image, 'src', phrases[i].image);
+}
 
 button.addEventListener('click', function () {
   let randomElement = getRandomElement(phrases);
   smoothly(phrase, 'textContent', randomElement.text);
-  smoothly(image, 'src', randomElement.image);
-
   if (randomElement.text.length > 40) {
     advice.style.fontSize = '33px';
   } else {
-    advice.style.fontSize = '42px';
+    advice.style.fontSize = '42px'
   }
+  smoothly(image, 'src', randomElement.image);
 });
-
-
-for (i=0; i<=2; i=i+1) {
-  smoothly(phrase, 'textContent', phrases[i].text);
-  smoothly(image, 'src', phrases[i].image);
-}
